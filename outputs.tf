89c38f7e8e45b3aa5dc4cbff9b063734e2876c2c
George Saffouri <george.saffouri@outlook.com>
2025-08-01 22:15:06 -0400

    WIP

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "cluster_iam_role_arn" {
+  value = module.eks.cluster_iam_role_arn
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
