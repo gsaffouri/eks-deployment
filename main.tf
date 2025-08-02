811c97ab6bc062ea2982e54e0ed736c4f7dce219
George Saffouri <george.saffouri@outlook.com>
2025-08-01 22:41:43 -0400

    WIP

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Provider configuration for AWS
+# This configuration sets up the AWS provider for Terraform with a specific version and region.
+# It requires Terraform version 1.5 or higher and uses AWS provider version 5.
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
+}
+
+data "terraform_remote_state" "bootstrap" {
+  backend = "s3"
+
+  config = {
+    bucket = "your-bootstrap-tf-state-bucket-name"
+    key    = "tf-bootstrap/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
