a4da1db95dac0268db2d8b01b131de590c386977
George Saffouri <george.saffouri@outlook.com>
2025-08-09 12:23:39 -0400

    bugs and fixes

--- main.tf
+++ main.tf
@@ -15,3 +15,36 @@ provider "aws" {
   region = "us-east-1" 
 }
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.29"
+
+  # Pull VPC info from aws-bootstrap remote state
+  vpc_id     = data.terraform_remote_state.bootstrap.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.bootstrap.outputs.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
+data "terraform_remote_state" "bootstrap" {
+  backend = "s3"
+  config = {
+    bucket         = "my-tf-state-bucket"
+    key            = "bootstrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
