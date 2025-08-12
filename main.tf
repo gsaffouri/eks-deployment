81a4f16e8a6d009b22f21b02f055c0de053ab3f6
George Saffouri <george.saffouri@outlook.com>
2025-08-11 21:36:35 -0400

    Everything is working :)

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"   # ✅ compatible with aws provider 4.x
+  version = "~> 4.0" # ✅ compatible with aws provider 4.x
 
   name = "${local.name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a","us-east-1b","us-east-1c"]
-  private_subnets = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -34,13 +34,13 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.21"  # ✅ works with aws provider 4.x
+  version = "~> 19.21" # ✅ works with aws provider 4.x
 
   cluster_name    = local.name
   cluster_version = local.cluster_version
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
   enable_irsa = true
 
   eks_managed_node_groups = {
